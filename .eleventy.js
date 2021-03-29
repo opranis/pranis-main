6f05d0c6957e42475001dc560816b2fefb3ff719
oliver <opranis@fakemail.com>
2021-03-30 02:19:42 +0300

    Setu up eleventy with TailwindCSS

---
+++ .eleventy.js
@@ -0,0 +1,8 @@
+module.exports = function (eleventyConfig) {
+eleventyConfig.setUseGitIgnore(false)
+
+eleventyConfig.addWatchTarget('./_tmp/style.css')
+
+    eleventyConfig.addPassthroughCopy({ './_tmp/style.css': './style.css' })
+
+};
---
+++ styles/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: {
+    content: ['_site/**/*.html'],
+    options: {
+      safelist: [],
+    },
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
