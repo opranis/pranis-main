647c0f7fc622ebe17bffc6c4b31b70b743bc9940
oliver <opranis@fakemail.com>
2021-03-31 17:24:30 +0300

    initial animation

--- _includes/scripts/cubes.js
+++ _includes/scripts/cubes.js
@@ -1,13 +1,18 @@
+var c = document.getElementById("cubes");
+var ctx = c.getContext("2d");
+var anim;
+
 drawCubes();
 window.addEventListener('resize', drawCubes);
 
 function drawCubes() {
-    var c = document.getElementById("cubes");
     c.width  = c.offsetWidth*4;
     c.height = c.offsetHeight*4;
-    
-    var ctx = c.getContext("2d");
-    
+    // window.cancelAnimationFrame(anim);
+    animate();
+}
+
+function drawGradient() {
     // Create gradient from corner to corner
     var grd = ctx.createLinearGradient(0, 0, c.width, c.height);
     grd.addColorStop(0, "#fa6a0f");
@@ -16,9 +21,21 @@ function drawCubes() {
     // Fill all canvas with gradient
     ctx.fillStyle = grd;
     ctx.fillRect(0, 0, c.width, c.height);
+}
 
-    let l = 400;
-    let gap = 3;
+var start;
+
+function animate(t) {
+    if (start === undefined) start = t;
+    const elapsed = t - start;
+
+    drawGradient();
+
+    // let l = 400;
+    // let gap = 3;
+
+    let l = elapsed/100;
+    let gap = 30 - elapsed/5000;
 
     let h = l*Math.sin(Math.PI/3);
     let horiz_dist = h+gap;
@@ -26,14 +43,16 @@ function drawCubes() {
 
     for (let i = 0; i <= c.height/(2*vert_dist) + 1; i++) {
         for (let j = 0; j <= c.width/(2*horiz_dist) + 1; j++){
-            drawLeaf(ctx, horiz_dist*j*2 - i%2*horiz_dist, vert_dist*i*2, l, 0, h);
-            drawLeaf(ctx, horiz_dist*(j*2+1) - i%2*horiz_dist, vert_dist*i*2, l, 1, h);
-            drawLeaf(ctx, horiz_dist*(j*2+0.5) - i%2*horiz_dist, vert_dist*(i*2+1), l, 2, h);
+            drawLeaf(horiz_dist*j*2 - i%2*horiz_dist, vert_dist*i*2, l, 0, h);
+            drawLeaf(horiz_dist*(j*2+1) - i%2*horiz_dist, vert_dist*i*2, l, 1, h);
+            drawLeaf(horiz_dist*(j*2+0.5) - i%2*horiz_dist, vert_dist*(i*2+1), l, 2, h);
         }
     }
+
+    anim = window.requestAnimationFrame(animate);
 }
 
-function drawLeaf(ctx, x, y, l, rot, h) {
+function drawLeaf(x, y, l, rot, h) {
     ctx.save();
     
     ctx.beginPath();
